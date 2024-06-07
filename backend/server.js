686d8512989f5fca40cb8663b94a40a44f010582
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2024-06-07 12:29:47 +0530

    setup with mongodb done

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+
 dotenv.config();
+connectDB();
+
 const PORT= process.env.PORT || 6000;
 
 const app= express();
-
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use('/api/users', userRoutes)
 
 app.get('/', (req,res) => {
@@ -18,7 +23,6 @@ app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server started on port :  ${PORT}`);
-    console.log("env is : ", process.env.NODE_ENV)
 });
 
 
